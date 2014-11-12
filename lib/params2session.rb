29a04dd956f32033ed5b4c3aee438a4bfc5102dd
Denis Peplin <denis.peplin@gmail.com>
2014-11-12 14:05:10 +0300

    Implement params2session methods.

--- lib/params2session.rb
+++ lib/params2session.rb
@@ -1,2 +1,25 @@
 module Params2session
+  module Helpers
+    def session_param(param_name)
+      session["#{controller_name}.#{param_name}"]
+    end
+  end
+
+  module Controller
+    def session_save(param_name)
+      session["#{controller_name}.#{param_name}"] = params[param_name] if params[param_name]
+    end
+
+    def session_update(param_name, value)
+      session["#{controller_name}.#{param_name}"] = value
+    end
+
+    def session_default(param_name, value)
+      update_session(param_name, value) unless session_param(param_name)
+    end
+  end
 end
+
+ActionController::Base.send(:include, Params2session::Helpers)
+ActionController::Base.send(:include, Params2session::Controller)
+ActionView::Base.send(:include, Params2session::Helpers)
